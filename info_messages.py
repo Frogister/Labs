e83584417260e309a7a9da4c8c84d641f22d6312
Frogister <123481423+Frogister@users.noreply.github.com>
2024-09-16 12:14:09 +0300

    rc2
    fully completed lab1

---
+++ info_messages.py
@@ -0,0 +1,25 @@
+class InfoMessages:
+    interface_input_info = \
+        "\nВведите желаемый пункт: "
+
+    file_select_success = \
+        f"\nФайл успешно выбран: "
+    file_upload_success = \
+        "\nФайл успешно загружен.\n"
+
+    file_upload_try_fix_info = \
+        "\nЕсли хотите попробовать использовать данный файл, нажмите (1), если нет - нажмите (0): "
+    file_upload_after_fix = \
+        "\nФайл загружен (неправильные строки убраны).\n"
+    file_upload_no_fix = \
+        "\nФайл не был загружен. Выберите другой файл или исправьте существующий. Вам потребуется выбрать файл снова.\n"
+
+    chose_region_recommendation = \
+        "\nВведите желаемый номер региона: "
+    chose_column_recommendation = \
+        "\nТеперь введите номер колонки: "
+    region_and_column_success = \
+        "\nРегион и колонка получены.\n"
+
+    exit_info = \
+        "\nВыход.\n"
